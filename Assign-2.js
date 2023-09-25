a72241741c10956a411c5cfa8f9b07117e2b5d2f
Abdulrehmanlodhi7 <144684224+Abdulrehmanlodhi7@users.noreply.github.com>
2023-09-25 16:27:28 +0500

    Add files via upload

---
+++ Assign-2.js
@@ -0,0 +1,3 @@
+var name1 = "Abdul Rehman";
+var message1 = "Hello! ".concat(name1, " would you like to learn python today?");
+console.log(message1);
---
+++ Assign-2.ts
@@ -0,0 +1,4 @@
+let name1:string = "Abdul Rehman"
+let message1:string = `Hello! ${name1} would you like to learn python today?`
+
+console.log(message1)
